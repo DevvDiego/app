306c6fd0f247043fe237afc6335da02935e5345b
Diego ans <diegovarela.202005gmail.com>
2023-12-24 12:40:52 -0600

    deploy

---
+++ src/routes/+page.svelte
@@ -0,0 +1,13 @@
+<script>
+    let counter = 1;
+</script>
+
+
+<h1>Counter: {counter}</h1>
+<button on:click={ () => {counter++} }>
+    add
+</button>
+
+<button on:click={ () => {counter--} }>
+    decrease
+</button>
