b10060144c67ec93cf70a44baba7cd8f91a760a4
Diego ans <diegovarela.202005gmail.com>
2023-12-24 13:30:15 -0600

    add output folders for build

--- svelte.config.js
+++ svelte.config.js
@@ -3,7 +3,10 @@ import adapter from '@sveltejs/adapter-static';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapter({			
+		adapter: adapter({
+			pages: "docs",
+            assets: "docs",
+			
 			// default options are shown. On some platforms
 			// these options are set automatically — see below
 			pages: 'build',
