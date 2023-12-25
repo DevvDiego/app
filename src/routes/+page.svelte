2cb44d7ea17a4f64058bc3991bd15bbc6ef24d04
Diego ans <diegovarela.202005gmail.com>
2023-12-24 19:40:59 -0600

    initial commit v2

---
+++ src/routes/+page.svelte
@@ -0,0 +1,15 @@
+<script>
+    export let counter = 0;
+</script>
+
+<h1>
+    Counter: {counter}
+</h1>
+
+<button on:click={ () => {counter++} }>
+    add
+</button>
+
+<button on:click={ () => {counter--} }>
+    sub
+</button>
