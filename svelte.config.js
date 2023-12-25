6eec1eb4e5f365b0e23f38e71f6793cb4b201b18
Diego ans <diegovarela.202005gmail.com>
2023-12-25 13:51:11 -0600

    cahne 2

--- svelte.config.js
+++ svelte.config.js
@@ -4,10 +4,12 @@ import adapter from '@sveltejs/adapter-static';
 const config = {
 	kit: {
 		adapter: adapter({
-			fallback: '404.html'
+			fallback: '404.html',
+			pages: "docs",
+            assets: "docs"
 		}),
 		paths: {
-			base: 'app/'
+			base: ''
 		}
 	}
 };
