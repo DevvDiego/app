9f27c9ec0066e10bceefab182d8297dd8d4f0090
Diego ans <diegovarela.202005gmail.com>
2023-12-25 21:26:02 -0600

    change

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,4 +3,42 @@
 </script>
 
 
+<nav>
+    <ul>
+        <li>
+            item
+        </li>
+        <li>
+            item
+        </li>
+    </ul>
+</nav>
+
 <slot />
+
+<style>
+    nav{
+        padding: 8px;
+
+        height: 70px;
+        width: 100%;
+
+        background-color: goldenrod;
+        color: black;
+    }
+
+    nav ul{
+        display: flex;
+        flex-flow: row nowrap;
+        justify-content: space-evenly;
+        align-items: center;
+    }
+
+    ul li{
+        text-decoration: none;
+        list-style: none;
+        background-color: rgb(194, 147, 28);
+        padding: 4px 8px;
+        border-radius: 4px;
+    }
+</style>
